5823a74de25ff85d47484f1ac70066a8f4257b5a
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-09-05 23:51:21 +0500

    sa

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
-// import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
